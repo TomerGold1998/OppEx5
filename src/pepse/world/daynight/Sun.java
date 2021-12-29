11945f72b4a3dec0532a51241a02b0a5d58fcaf8
Tomer Goldberg <tomergoldbergpsn@gmail.com>
2021-12-29 19:46:11 +0200

    added the sun movement across the angle

--- src/pepse/world/daynight/Sun.java
+++ src/pepse/world/daynight/Sun.java
@@ -2,14 +2,19 @@ package pepse.world.daynight;
 
 import danogl.GameObject;
 import danogl.collisions.GameObjectCollection;
+import danogl.components.CoordinateSpace;
+import danogl.components.Transition;
 import danogl.gui.rendering.OvalRenderable;
 import danogl.util.Vector2;
+import pepse.transitions.AngleChangeTransitionExecutor;
 
 import java.awt.*;
 
 public class Sun {
 
     private static int SUN_SIZE = 200;
+    private static int SUN_INITAL_ANGLE = 0;
+    private static int SUN_FINAL_ANGLE = 360;
 
     public static GameObject create(
             GameObjectCollection gameObjects,
@@ -17,14 +22,28 @@ public class Sun {
             Vector2 windowDimensions,
             float cycleLength) {
 
+        var transition = new AngleChangeTransitionExecutor(
+                SUN_INITAL_ANGLE,
+                SUN_FINAL_ANGLE,
+                windowDimensions,
+                Transition.LINEAR_INTERPOLATOR_FLOAT,
+                Transition.TransitionType.TRANSITION_LOOP,
+                null);
+
         var sunRenderable = new OvalRenderable(Color.YELLOW);
-        var gameObject = new GameObject(Vector2.ZERO,
+        var sun = new GameObject(Vector2.ZERO,
                 new Vector2(SUN_SIZE, SUN_SIZE),
                 sunRenderable);
+        sun.setCoordinateSpace(CoordinateSpace.CAMERA_COORDINATES);
+        sun.setCenter(new Vector2(windowDimensions.x() / 4,
+                windowDimensions.y() - windowDimensions.y() / 4));
+        sun.setTag("sun");
 
-        gameObject.setCenter(new Vector2(windowDimensions.x() / 2, windowDimensions.y() / 2));
-        gameObjects.addGameObject(gameObject, layer);
-        return gameObject;
+        transition.executeTransition(cycleLength, sun);
+
+        gameObjects.addGameObject(sun, layer);
+
+        return sun;
     }
 
 }
