a12e51e56f460802d12fb644ae68e56a872c7ace
Tomer Goldberg <tomergoldbergpsn@gmail.com>
2021-12-29 17:23:55 +0200

    creating the sun object

--- src/pepse/world/daynight/Sun.java
+++ src/pepse/world/daynight/Sun.java
@@ -1,5 +1,30 @@
 package pepse.world.daynight;
 
+import danogl.GameObject;
+import danogl.collisions.GameObjectCollection;
+import danogl.gui.rendering.OvalRenderable;
+import danogl.util.Vector2;
+
+import java.awt.*;
+
 public class Sun {
+
+    private static int SUN_SIZE = 200;
+
+    public static GameObject create(
+            GameObjectCollection gameObjects,
+            int layer,
+            Vector2 windowDimensions,
+            float cycleLength) {
+
+        var sunRenderable = new OvalRenderable(Color.YELLOW);
+        var gameObject = new GameObject(Vector2.ZERO,
+                new Vector2(SUN_SIZE, SUN_SIZE),
+                sunRenderable);
+
+        gameObject.setCenter(new Vector2(windowDimensions.x() / 2, windowDimensions.y() / 2));
+        gameObjects.addGameObject(gameObject, layer);
+        return gameObject;
+    }
 
 }
