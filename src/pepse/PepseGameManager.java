49f26e508369b90f1404e1303e277ef09e60931e
yukhnovetsky.ruth <yukhnovetsky.ruth@gmail.com>
2021-12-30 16:15:14 +0200

    moved consts to own configuration pakagae

--- src/pepse/PepseGameManager.java
+++ src/pepse/PepseGameManager.java
@@ -2,12 +2,14 @@ package pepse;
 
 import danogl.GameManager;
 import danogl.collisions.GameObjectCollection;
-import danogl.collisions.Layer;
 import danogl.gui.ImageReader;
 import danogl.gui.SoundReader;
 import danogl.gui.UserInputListener;
 import danogl.gui.WindowController;
 import danogl.util.Vector2;
+import pepse.configuration.GameLayers;
+import pepse.configuration.GameObjectsConfiguration;
+import pepse.configuration.TransitionConfiguration;
 import pepse.world.Sky;
 import pepse.world.Terrain;
 import pepse.world.daynight.Night;
@@ -19,7 +21,7 @@ import java.awt.*;
 public class PepseGameManager extends GameManager {
     private GameObjectCollection gameObjects;
     private Vector2 windowDim;
-    private static Color SUN_HALO_COLOR = new Color(255, 255, 0, 20);
+
 
     @Override
     public void initializeGame(ImageReader imageReader,
@@ -29,12 +31,15 @@ public class PepseGameManager extends GameManager {
         super.initializeGame(imageReader, soundReader, inputListener, windowController);
         this.gameObjects = gameObjects();
         this.windowDim = windowController.getWindowDimensions();
-        Terrain terrain = new Terrain(this.gameObjects, Layer.STATIC_OBJECTS, this.windowDim, 42);
-        terrain.createInRange(0, (int) (this.windowDim.x() + 100));
-        Sky.create(this.gameObjects, this.windowDim, Layer.BACKGROUND);
-        Night.create(gameObjects, Layer.FOREGROUND, windowDim, 30);
-        var sun = Sun.create(gameObjects, Layer.BACKGROUND +1, windowDim, 25);
-        SunHalo.create(gameObjects, Layer.BACKGROUND + 2, sun, SUN_HALO_COLOR);
+
+        Terrain terrain = new Terrain(this.gameObjects, GameLayers.BLOCK_LAYER, this.windowDim, GameObjectsConfiguration.TERRAIN_SEED);
+        terrain.createInRange(0, (int) (this.windowDim.x() + GameObjectsConfiguration.TERRAIN_X_BUFFER));
+
+        Sky.create(this.gameObjects, this.windowDim, GameLayers.SKY_LAYER);
+        Night.create(gameObjects, GameLayers.NIGHT_LAYER, windowDim, TransitionConfiguration.NIGHT_CYCLE_LENGTH);
+
+        var sun = Sun.create(gameObjects, GameLayers.SUN_LAYER, windowDim, TransitionConfiguration.SUN_CYCLE_LENGTH);
+        SunHalo.create(gameObjects, GameLayers.SUN_HALO_LAYER, sun, GameObjectsConfiguration.SUN_HALO_COLOR);
     }
 
     public static void main(String[] args) {
---
+++ src/pepse/configuration/GameObjectsConfiguration.java
@@ -0,0 +1,16 @@
+package pepse.configuration;
+
+import java.awt.*;
+
+/**
+ * Used in order to store game objects consts
+ */
+public class GameObjectsConfiguration {
+    //SUN
+    public final static  Color SUN_HALO_COLOR = new Color(255, 255, 0, 20);
+
+    //TERRAIN
+    public final static int TERRAIN_SEED = 42;
+    public final static int TERRAIN_X_BUFFER = 100;
+
+}
