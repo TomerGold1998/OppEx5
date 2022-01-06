ee87ffe8153a2bb3f8c2495a0b3e56c025f2b844
yukhnovetsky.ruth <yukhnovetsky.ruth@gmail.com>
2022-01-06 11:56:58 +0200

    api documintaion finished

--- src/pepse/world/ReactingBlock.java
+++ src/pepse/world/ReactingBlock.java
@@ -4,7 +4,7 @@ import danogl.gui.rendering.Renderable;
 import danogl.util.Vector2;
 
 /**
- *
+ *reacting block class
  */
 public class ReactingBlock extends Block {
     public ReactingBlock(Vector2 topLeftCorner, Renderable renderable) {
